a68c0f4b2907d36e9a27c1b5924692bd4f29c124
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2025-03-16 17:01:01 -0700

    item instance variables under test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
+  it 'has a name and volume' do
+    item = Item.new("fake", 50)
+    expect(item.name).to eq "fake"
+    expect(item.volume).to eq 50
+  end
 
 end
